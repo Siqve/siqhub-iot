9b302bb8063ddd9ac12ae0eb069f08484dfd99dd
Siqve <aadne8@gmail.com>
2023-10-24 15:19:21 +0200

    Updated ColorUtils to also convert HSV into RgbColor

--- Arduino/src/utilities/ColorUtils.cpp
+++ Arduino/src/utilities/ColorUtils.cpp
@@ -1,7 +1,6 @@
 #include <sstream>
 #include "ColorUtils.h"
 #include "Adafruit_NeoPixel.h"
-//#include "Adafruit_NeoPixel.h"
 
 namespace ColorUtils {
 
@@ -13,7 +12,6 @@ namespace ColorUtils {
         stringstream >> std::hex >> value;
         return value;
     }
-
 
     uint32_t HSVToColor(uint16_t hue, uint8_t sat, uint8_t val) {
         uint8_t r, g, b;
@@ -84,14 +82,86 @@ namespace ColorUtils {
         uint32_t v1 = 1 + val;  // 1 to 256; allows >>8 instead of /255
         uint16_t s1 = 1 + sat;  // 1 to 256; same reason
         uint8_t s2 = 255 - sat; // 255 to 0
+
+        // (r * s1)
+        //  This scales the red according to the saturation s1
+        // >> 8)
+        //  Since we multiplied a uint8_t with a uint16_t (max val 256), we now have a uint16_t with the left 8 bits representing the most significant color values.
+        //  Bitwise shifting them by 8 bits makes the 8 least significant bits now represent the value
+        // + s2)
+        //  This adds the second saturation.
+        // * v1)
+        //  This multiplies with the value (representing brightness)
+        // & 0xff00)
+        //  Since we multiplied with a uint16_t we get a result that is 2 bytes. Now we only want the byte that is stored in the most significant position (leftmost)
+        //  therefore we mask it with 0xff00, which discards the 8 least significant bytes
+        // << 8 | | >> 8
+        //  After the masking we need to move the byte to it's place in the uint32_t (4 bytes)
         return ((((((r * s1) >> 8) + s2) * v1) & 0xff00) << 8) |
                (((((g * s1) >> 8) + s2) * v1) & 0xff00) |
                (((((b * s1) >> 8) + s2) * v1) >> 8);
     }
 
+    RgbColor HSVToRgbColor(uint16_t hue, uint8_t sat, uint8_t val) {
+        uint8_t r, g, b;
+        hue = (hue * 1530L + 32768) / 65536;
+        if (hue < 510) { // Red to Green-1
+            b = 0;
+            if (hue < 255) { //   Red to Yellow-1
+                r = 255;
+                g = hue;       //     g = 0 to 254
+            } else {         //   Yellow to Green-1
+                r = 510 - hue; //     r = 255 to 1
+                g = 255;
+            }
+        } else if (hue < 1020) { // Green to Blue-1
+            r = 0;
+            if (hue < 765) { //   Green to Cyan-1
+                g = 255;
+                b = hue - 510;  //     b = 0 to 254
+            } else {          //   Cyan to Blue-1
+                g = 1020 - hue; //     g = 255 to 1
+                b = 255;
+            }
+        } else if (hue < 1530) { // Blue to Red-1
+            g = 0;
+            if (hue < 1275) { //   Blue to Magenta-1
+                r = hue - 1020; //     r = 0 to 254
+                b = 255;
+            } else { //   Magenta to Red-1
+                r = 255;
+                b = 1530 - hue; //     b = 255 to 1
+            }
+        } else { // Last 0.5 Red (quicker than % operator)
+            r = 255;
+            g = b = 0;
+        }
+
+        // Apply saturation and value to R,G,B, pack into 32-bit result:
+        uint32_t v1 = 1 + val;  // 1 to 256; allows >>8 instead of /255
+        uint16_t s1 = 1 + sat;  // 1 to 256; same reason
+        uint8_t s2 = 255 - sat; // 255 to 0
+
+        // (r * s1)
+        //  This scales the red according to the saturation s1
+        // >> 8)
+        //  Since we multiplied a uint8_t with a uint16_t (max val 256), we now have a uint16_t with the left 8 bits representing the most significant color values.
+        //  Bitwise shifting them by 8 bits makes the 8 least significant bits now represent the value
+        // + s2)
+        //  This adds the second saturation.
+        // * v1)
+        //  This multiplies with the value (representing brightness)
+        // >> 8
+        //  After the masking we have the red byte stored in the leftmost of 16 bytes. We only want to return the rightmost 8 bytes, therefore we shift them right
+        uint8_t red = ((((r * s1) >> 8) + s2) * v1) >> 8;
+        uint8_t green = (((g * s1) >> 8) + s2) * v1 >> 8;
+        uint8_t blue = (((b * s1) >> 8) + s2) * v1 >> 8;
+        return RgbColor(red, green, blue);
+    }
+
 
     uint32_t Gamma32(uint32_t x) {
-        uint8_t *y = (uint8_t *) &x;
+        uint8_t* y = (uint8_t*) &x;
         // All four bytes of a 32-bit value are filtered even if RGB (not WRGB),
         // to avoid a bunch of shifting and masking that would be necessary for
         // properly handling different endianisms (and each byte is a fairly
@@ -113,12 +183,19 @@ namespace ColorUtils {
         return (uint32_t) r << 16 | (uint16_t) g << 8 | b;
     }
 
-    int *colorToRGB(uint32_t color) {
+    int* colorToRGB(uint32_t color) {
         int red = (uint8_t) (color >> 16);
         int green = (uint8_t) (color >> 8);
         int blue = (uint8_t) (color);
         static int colors[3] = {red, green, blue};
         return colors;
+    }
+
+    RgbColor colorToRgbColor(uint32_t color) {
+        int red = (uint8_t) (color >> 16);
+        int green = (uint8_t) (color >> 8);
+        int blue = (uint8_t) (color);
+        return RgbColor(red, green, blue);
     }
 
 }
--- Arduino/src/utilities/ColorUtils.h
+++ Arduino/src/utilities/ColorUtils.h
@@ -1,14 +1,17 @@
 #ifndef ColorUtils_h
 #define ColorUtils_h
 
+#include "NeoPixelBus.h"
 
 namespace ColorUtils {
     extern uint32_t hexStringToColor(const char* hexString);
     extern uint32_t HSVToColor(uint16_t hue, uint8_t sat, uint8_t val);
+    extern RgbColor HSVToRgbColor(uint16_t hue, uint8_t sat, uint8_t val);
     extern uint32_t Gamma32(uint32_t x);
     extern uint8_t Gamma8(uint8_t x);
     extern uint32_t rgbToColor(uint8_t r, uint8_t g, uint8_t b);
     extern int * colorToRGB(uint32_t color);
+    extern RgbColor colorToRgbColor(uint32_t color);
 }
 
 #endif
