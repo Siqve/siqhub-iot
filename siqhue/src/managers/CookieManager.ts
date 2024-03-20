74965e7bb828833b8ce2f93285e2f0ee3ed59943
Siqve <aadne8@gmail.com>
2024-03-20 20:22:26 +0100

    Fixed cookieManager

--- siqhue/src/managers/CookieManager.ts
+++ siqhue/src/managers/CookieManager.ts
@@ -1,21 +1,22 @@
 import { cookies } from "next/headers";
 
-const cookieStore = cookies();
 
 export const getCookie = (cookie: string) => {
+    const cookieStore = cookies();
     return cookieStore.get(cookie);
 };
 
 export const hasCookie = (cookie: string) => {
+    const cookieStore = cookies();
     return cookieStore.has(cookie)
 };
 
 export const setCookie = (cookie: string, data: string) => {
-    // const cookieStore = cookies();
+    const cookieStore = cookies();
     cookieStore.set(cookie, data);
 };
 
 export const deleteCookie = (cookie: string) => {
-    // const cookieStore = cookies();
+    const cookieStore = cookies();
     cookieStore.delete(cookie);
 };
