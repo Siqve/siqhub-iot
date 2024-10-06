acf5805e8b4236cf5f1b166b25e4ca6a3575bb8c
adnesiqve <adne.siqveland@bouvet.no>
2024-10-06 19:29:37 +0200

    Minor update to LEDMode

--- src/hardware/LED/modes/StaticLEDMode.h
+++ src/hardware/LED/modes/StaticLEDMode.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include "LEDMode.h"
+#include "ArduinoJson.h"
 
 class StaticLEDMode : public LEDMode {
 public:
     StaticLEDMode(NeoPixelBus<NeoBrgFeature, Neo800KbpsMethod>& LEDStrip, std::function<void(int)> setFPS);
-    void onActivate() override;
+    void initialize(const JsonDocument& settings) override;
     void loop() override;
     void onUpdate(const RequestWrapper& request) override;
     void onDebugCommand(const std::string& command) override;
