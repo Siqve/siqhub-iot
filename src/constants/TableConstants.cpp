7b450ea18907f394260b484fe7e9126c20f8526b
Siqve <aadne8@gmail.com>
2024-11-14 17:59:19 +0100

    Added Color constants

--- src/constants/TableConstants.cpp
+++ src/constants/TableConstants.cpp
@@ -3,12 +3,16 @@
 namespace TableConstants {
 
     namespace Device {
-
         const std::string TABLE_NAME = "device";
         const std::string COLUMN_ID = "id";
         const std::string COLUMN_TYPE = "type";
         const std::string COLUMN_SETTINGS_JSON = "settings_json";
+    }
 
+    namespace Color {
+        const std::string TABLE_NAME = "color";
+        const std::string COLUMN_ID = "id";
+        const std::string COLUMN_HEX = "hex";
     }
 
 }
--- src/constants/TableConstants.h
+++ src/constants/TableConstants.h
@@ -5,12 +5,16 @@
 namespace TableConstants {
 
     namespace Device {
-
         extern const std::string TABLE_NAME;
         extern const std::string COLUMN_ID;
         extern const std::string COLUMN_TYPE;
         extern const std::string COLUMN_SETTINGS_JSON;
+    }
 
+    namespace Color {
+        extern const std::string TABLE_NAME;
+        extern const std::string COLUMN_ID;
+        extern const std::string COLUMN_HEX;
     }
 
 }
